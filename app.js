a00d108040b760d0cec45ffc9a4f830fe97adb46
MVBarrionuevo <vikibarrionuevo@gmail.com>
2021-09-23 12:19:10 -0300

    agrego css + las pantallas de login y register

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require("express");
-const app= express(); 
+const app = express();
 
-const path = require("path"); 
+const path = require("path");
 
 const publicPath = path.resolve(__dirname, "./public");
 
-app.use(express.static(publicPath)); 
+app.use(express.static(publicPath));
 
 app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000");
 });
 
 
-app.get("/", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "./views/home.html")); 
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
 
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.post("/register", (req, res) => {
+    res.redirect("/");
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
+app.post("/login", (req, res) => {
+    res.redirect("/");
+});
