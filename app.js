db89986b2dd587c58662eaa3e6765255b4bd750f
MVBarrionuevo <vikibarrionuevo@gmail.com>
2021-09-28 19:43:23 -0300

    modifico procfile + elimino node_modules

--- app.js
+++ app.js
@@ -7,9 +7,16 @@ const publicPath = path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+//Cambio este bloque para Heroku
+/*app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000");
 });
+*/
+
+//Agrego este bloque nuevo para HEROKU:
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Servidor corriendo en el puerto 3000");
+})
 
 
 app.get("/", (req, res) => {
@@ -19,7 +26,6 @@ app.get("/", (req, res) => {
 app.get("/register", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/register.html"));
 });
-
 app.post("/register", (req, res) => {
     res.redirect("/");
 });
